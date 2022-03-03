0fa48322cfe8aabdf8c3fa2f7d261cb7c3b68e2c
Mike Deegan <Deegan_Mike@bah.com>
2022-03-03 12:36:33 -0500

    Initial commit

---
+++ model.py
@@ -0,0 +1,45 @@
+# 1. Library imports
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+from pydantic import BaseModel
+import joblib
+
+
+# 2. Class which describes a single flower measurements
+class IrisSpecies(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+# 3. Class for training the model and making predictions
+class IrisModel:
+    # 6. Class constructor, loads the dataset and loads the model
+    #    if exists. If not, calls the _train_model method and
+    #    saves the model
+    def __init__(self):
+        self.df = pd.read_csv('iris.csv')
+        self.model_fname_ = 'iris_model.pkl'
+        try:
+            self.model = joblib.load(self.model_fname_)
+        except Exception as _:
+            self.model = self._train_model()
+            joblib.dump(self.model, self.model_fname_)
+
+    # 4. Perform model training using the RandomForest classifier
+    def _train_model(self):
+        x = self.df.drop('Species', axis=1)
+        y = self.df['Species']
+        rfc = RandomForestClassifier()
+        model = rfc.fit(x, y)
+        return model
+
+    # 5. Make a prediction based on the user-entered data
+    #    Returns the predicted species with its respective probability
+    def predict_species(self, sepal_length, sepal_width, petal_length, petal_width):
+        data_in = [[sepal_length, sepal_width, petal_length, petal_width]]
+        prediction = self.model.predict(data_in)
+        probability = self.model.predict_proba(data_in).max()
+        return prediction[0], probability
